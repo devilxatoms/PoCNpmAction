e2922c15a8e69185ae7fa6bdd1fb0410972b5295
Brayan Caldera <ing.brayan.cm@gmail.com>
2024-02-26 02:44:56 -0600

    update actions

--- index.js
+++ index.js
@@ -15,16 +15,23 @@ const { parseXmlReport } = require("./src/parseXmlReport");
   core.setFailed(error.message);
 } */
 
+function transformToTableData(operations) {
+  return operations.flatMap((operation) => {
+    const operationName = operation.Name;
+    const items = Array.isArray(operation.Item) ? operation.Item : [operation.Item];
+
+    return items.map((item) => [operationName, item.Value, item.Type]);
+  });
+}
+
 //  read xml file
 const xmlFilePath = "dummyData/dummy.xml";
 
 parseXmlReport(xmlFilePath)
   .then((result) => {
-    console.log(result.DeploymentReport.Operations);
-    console.log(
-      "JSON DATA: ",
-      JSON.stringify(result.DeploymentReport.Operations)
-    );
+    const tableData = transformToTableData(result.DeploymentReport.Operations);
+    console.log("Table Data:", tableData);
+
   })
   .catch((err) => {
     console.error(err);
