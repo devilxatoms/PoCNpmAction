1312c3c56b261d5b7baa0d66ef50f393cfae61d3
Brayan Caldera <ing.brayan.cm@gmail.com>
2024-02-26 03:55:41 -0600

    update actions

--- dist/index.js
+++ dist/index.js
@@ -33241,7 +33241,6 @@ const { parseXmlReport } = __nccwpck_require__(3877);
 } */
 
 function transformToTableData(operations) {
-  console.log("Operations: ", operations);
   const tableData = [];
   operations.Operation.forEach((operation) => {
     if (Array.isArray(operation.Item)) {
@@ -33249,7 +33248,11 @@ function transformToTableData(operations) {
         tableData.push([operation.Name, item.Value, item.Type]);
       });
     } else {
-      tableData.push([operation.Name, operation.Item.Value, operation.Item.Type]);
+      tableData.push([
+        operation.Name,
+        operation.Item.Value,
+        operation.Item.Type,
+      ]);
     }
   });
   return tableData;
@@ -33260,41 +33263,29 @@ const xmlFilePath = "dummyData/dummy.xml";
 
 parseXmlReport(xmlFilePath)
   .then((result) => {
-    console.log("Result: ", result);
-    console.log("Result operations: ", result.DeploymentReport.Operations);
     const tableData = transformToTableData(result.DeploymentReport.Operations);
     console.log(">>>Table Data:", tableData);
+    async function run() {
+      await core.summary
+        .addHeading("SQL Changes Applied")
+        .addTable([
+          [
+            { data: "Operation Name", header: true },
+            { data: "Item Value", header: true },
+            { data: "Type", header: true },
+          ],
+          ...tableData,
+          // Agrega más filas según sea necesario
+        ])
+        .write();
+    }
+
+    // Call the asynchronous function
+    run();
   })
   .catch((err) => {
     console.error(err);
   });
-
-/* async function run() {
-  await core.summary
-    .addHeading("SQL Changes Applied")
-    .addTable([
-      [
-        { data: "Operation Name", header: true },
-        { data: "Item Value", header: true },
-        { data: "Type", header: true },
-      ],
-      ["Create", "[dbo].[DummyTable1]", "SqlTable"],
-      ["Alter", "[dbo].[DummyFunction1]", "SqlScalarFunction"],
-      ["Alter", "[dbo].[DummyFunction2]", "SqlScalarFunction"],
-      ["Alter", "[dbo].[DummyFunction3]", "SqlScalarFunction"],
-      ["Alter", "[dbo].[DummyFunction4]", "SqlScalarFunction"],
-      ["Alter", "[dbo].[DummyFunction5]", "SqlScalarFunction"],
-      ["Alter", "[dbo].[DummyTable2]", "SqlTable"],
-      ["Alter", "[dbo].[DummyTable3]", "SqlTable"],
-      ["Refresh", "[dbo].[DummyProcedure1]", "SqlProcedure"],
-      ["Refresh", "[dbo].[DummyProcedure2]", "SqlProcedure"],
-      // Agrega más filas según sea necesario
-    ])
-    .write();
-}
-
-// Call the asynchronous function
-run(); */
 
 })();
 
--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const { parseXmlReport } = require("./src/parseXmlReport");
 } */
 
 function transformToTableData(operations) {
-  console.log("Operations: ", operations);
   const tableData = [];
   operations.Operation.forEach((operation) => {
     if (Array.isArray(operation.Item)) {
@@ -24,7 +23,11 @@ function transformToTableData(operations) {
         tableData.push([operation.Name, item.Value, item.Type]);
       });
     } else {
-      tableData.push([operation.Name, operation.Item.Value, operation.Item.Type]);
+      tableData.push([
+        operation.Name,
+        operation.Item.Value,
+        operation.Item.Type,
+      ]);
     }
   });
   return tableData;
@@ -35,38 +38,26 @@ const xmlFilePath = "dummyData/dummy.xml";
 
 parseXmlReport(xmlFilePath)
   .then((result) => {
-    console.log("Result: ", result);
-    console.log("Result operations: ", result.DeploymentReport.Operations);
     const tableData = transformToTableData(result.DeploymentReport.Operations);
     console.log(">>>Table Data:", tableData);
+    async function run() {
+      await core.summary
+        .addHeading("SQL Changes Applied")
+        .addTable([
+          [
+            { data: "Operation Name", header: true },
+            { data: "Item Value", header: true },
+            { data: "Type", header: true },
+          ],
+          ...tableData,
+          // Agrega más filas según sea necesario
+        ])
+        .write();
+    }
+
+    // Call the asynchronous function
+    run();
   })
   .catch((err) => {
     console.error(err);
   });
-
-/* async function run() {
-  await core.summary
-    .addHeading("SQL Changes Applied")
-    .addTable([
-      [
-        { data: "Operation Name", header: true },
-        { data: "Item Value", header: true },
-        { data: "Type", header: true },
-      ],
-      ["Create", "[dbo].[DummyTable1]", "SqlTable"],
-      ["Alter", "[dbo].[DummyFunction1]", "SqlScalarFunction"],
-      ["Alter", "[dbo].[DummyFunction2]", "SqlScalarFunction"],
-      ["Alter", "[dbo].[DummyFunction3]", "SqlScalarFunction"],
-      ["Alter", "[dbo].[DummyFunction4]", "SqlScalarFunction"],
-      ["Alter", "[dbo].[DummyFunction5]", "SqlScalarFunction"],
-      ["Alter", "[dbo].[DummyTable2]", "SqlTable"],
-      ["Alter", "[dbo].[DummyTable3]", "SqlTable"],
-      ["Refresh", "[dbo].[DummyProcedure1]", "SqlProcedure"],
-      ["Refresh", "[dbo].[DummyProcedure2]", "SqlProcedure"],
-      // Agrega más filas según sea necesario
-    ])
-    .write();
-}
-
-// Call the asynchronous function
-run(); */
