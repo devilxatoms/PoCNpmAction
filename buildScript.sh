d3a9a8a5b601be175d412b3b633c15dc0b62045f
Brayan Caldera <ing.brayan.cm@gmail.com>
2024-02-27 03:00:58 -0600

    update action with script version:

--- buildScript.sh
+++ buildScript.sh
@@ -5,17 +5,17 @@ ENV_FILE=".env"
 
 # Verificar si el archivo de versión existe
 if [ -f "$ENV_FILE" ]; then
-    # Leer la versión actual desde el archivo
+    # Cargar las variables de entorno desde el archivo
     source "$ENV_FILE"
 else
-    # Si el archivo no existe, establecer la versión inicial
-    SCRIPT_VERSION=1.0
+    # Si el archivo no existe, establecer la versión inicial como 1.0
+    export SCRIPT_VERSION=1.0
 fi
 
 # Incrementar la versión
-SCRIPT_VERSION=$(echo "$SCRIPT_VERSION + 0.1" | bc)
+export SCRIPT_VERSION=$(echo "$SCRIPT_VERSION + 0.1" | bc)
 
-# Guardar la nueva versión en el archivo .env
+# Guardar la versión en el archivo .env
 echo "SCRIPT_VERSION=$SCRIPT_VERSION" > "$ENV_FILE"
 
 # Instalar dependencias y compilar el script
